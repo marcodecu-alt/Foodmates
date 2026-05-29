57dc1ca9ef60e6e6d7180ee2526aeab377cc5a64
marcodecu-alt <marcodecu@gmail.com>
2026-05-29 14:39:39 +0100

    fix: cast Uint8Array to BufferSource to satisfy PushManager types

--- lib/pushNotifications.ts
+++ lib/pushNotifications.ts
@@ -33,7 +33,7 @@ export async function registerAndSubscribe(): Promise<void> {
     if (!sub) {
       sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as BufferSource,
       });
     }
 
